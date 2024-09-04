eacd2d1ce50ffa36e61ac4a6558d96a81358e674
Kartik Raut <userkartik01@gmail.com>
2024-09-04 11:27:22 +0530

    browser changes

--- src/main/java/Browser/Chrome.java
+++ src/main/java/Browser/Chrome.java
@@ -1,34 +1,46 @@
 package Browser;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Chrome {
-	
-	public static void main(String[] args) throws InterruptedException {
-		
-	
-	//System.setProperty("webdriver.chrome.driver","");
-	
-	//WebDriver driver = new ChromeDriver();
-	
-//	driver.get("www.google.com");
-	
-	WebDriverManager.chromedriver().setup();
-	
-	
-	WebDriver driver = new ChromeDriver();
-	
-	Thread.sleep(5000);
-	
-	driver.manage().window().maximize();
-	
-	driver.get("https://github.com/0KartikRaut0/Automation_Selenium/blob/main/pom.xml");
-	
-	
-	
+
+	static WebDriver driver;
+
+	public static WebDriver start() {
+
+//		System.setProperty("webdriver.chrome.driver", "");
+
+		WebDriverManager.edgedriver().setup();
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://github.com/0KartikRaut0/Automation_Selenium/blob/main/pom.xml");
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		return driver;
+
+	}
+
+	public static WebDriver close() {
+
+		driver.close();
+		System.out.println("jh");
+
+		return driver;
 	}
 	
+	public static void main(String[] args) {
+		
+		Chrome.start();
+		Chrome.close();
+	}
+
 }
---
+++ src/main/java/CommonFacotry/BaseClass.java
@@ -0,0 +1,7 @@
+package CommonFacotry;
+
+public class BaseClass {
+	
+	
+
+}
